b5321d80ee119ba4316b256d0fac33c49c9243f4
Margo <mrnagapetyan@mail.ru>
2021-10-17 20:39:34 +0300

    osnova

--- in.js
+++ in.js
@@ -3,8 +3,8 @@ var btn = document.querySelector('#btn');
 var out = document.querySelector('#out');
 var cena = document.querySelector('#cena');
 var kol = document.querySelector('#kol');
-var c=cena.value;
-var k=kol.value;
+//var c=cena.value;
+//var k=kol.value;
 
 function oc()
 {  document.querySelector('#cena').style.borderColor="red";}
@@ -47,3 +47,92 @@ else if(reg2.test(kol.value)===true) {on();ok();}
 }
     else {out.innerHTML = pro;ob();} }
  });
+
+
+// Элементы формы
+const squareInput = document.querySelector('#square-input');
+const inputs = document.querySelectorAll('input');
+
+//селектор
+const select = document.getElementsByName('myselect')
+
+// Радиокнопки
+const radioType = document.querySelectorAll('input[name="type"]');
+const chType = document.querySelectorAll('input[name="ceiling"]');
+
+// Чекбокс
+const ceilings = document.querySelector('input[name="ceiling"]');
+
+const basePrice = 0;
+const totalPriceElement = document.querySelector('#total-price');
+
+let selectnow=1;
+let radios = document.getElementById('radios');
+let checkbox = document.getElementById('checkbox');
+
+function calculate() {
+let totalPrice
+const reg = /[^0-9]|\b0[0-9]+/;
+let price = document.getElementsByName("field1");
+let amount = document.getElementsByName("field2");
+if(reg.test(parseInt(squareInput.value)) === true){
+alert("Повторите ввод данных");
+totalPriceElement.innerText = "0";
+}
+else{
+if(selectnow==1){ //велосипед
+totalPrice=50;
+totalPrice *= parseInt(squareInput.value);
+}
+else if(selectnow==2){ //автомобиль
+totalPrice=70;
+for (const radio of radioType) {
+if (radio.checked) {
+totalPrice = totalPrice + parseFloat(radio.value);
+}
+}
+totalPrice *= parseInt(squareInput.value);
+}
+else if(selectnow==3){ //дом
+totalPrice=100;
+for (const ceilings of chType) {
+if (ceilings.checked) {
+totalPrice = totalPrice + parseInt(ceilings.value);
+}}
+totalPrice *= parseInt(squareInput.value);
+}
+const formatter = new Intl.NumberFormat('ru');
+totalPriceElement.innerText = formatter.format(totalPrice);
+}
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+calculate();
+radios.style.display = "none";
+checkbox.style.display = "none";
+select[0].addEventListener("change", function(event) {
+let select = event.target;
+if (select.value == "1") {
+radios.style.display = "none";
+checkbox.style.display = "none";
+selectnow = 1;
+}
+else if(select.value == "2") {
+radios.style.display = "block";
+checkbox.style.display = "none";
+selectnow = 2;
+}
+else {
+radios.style.display = "none";
+checkbox.style.display = "block";
+selectnow = 3;
+}
+calculate();
+});
+
+for (const input of inputs) {
+input.addEventListener('input', function () {
+calculate();
+});
+}
+});
