687c17677abe961074c0a5fb8c78bf422314c49c
Margo <mrnagapetyan@mail.ru>
2021-10-21 23:43:06 +0300

    osnova

--- in.js
+++ in.js
@@ -145,3 +145,30 @@ calculate();
 });
 }
 });
+$(document).ready(function(){
+$('.slider').slick({
+  arrows:true,
+   dots: true,
+   adaptiveHeight:true,
+   slidesToShow:4,
+   slidesToScroll:4,
+   easing: 'ease',
+   infinite:true,
+   autoplay:true,
+   autoplaySpeed:2500,
+   pauseOnFocus:true,
+   pauseOnHover: true,
+   pauseOnDotsHover:true,
+   touchThreshold:10,
+   waitForAnimate:false,
+   responsive:[
+     {
+       breakpoint: 480,
+      settings:{
+        slidesToShow:2,
+        slidesToScroll:2,
+      }
+     }
+   ],
+});
+});
